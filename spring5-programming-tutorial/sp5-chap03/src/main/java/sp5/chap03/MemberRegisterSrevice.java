43271162733bf87edf2911d1e3379a5ca4294888
gyung688 <64251036+gyung688@users.noreply.github.com>
2024-03-26 00:09:15 +0900

    spring

--- spring5-programming-tutorial/sp5-chap03/src/main/java/sp5/chap03/MemberRegisterSrevice.java
+++ spring5-programming-tutorial/sp5-chap03/src/main/java/sp5/chap03/MemberRegisterSrevice.java
@@ -5,20 +5,26 @@ import java.time.LocalDateTime;
 
 public class MemberRegisterSrevice {
 
+	private MemberDao memberDao;
+	
+	public MemberRegisterSrevice(MemberDao memberDao) {
+		this.memberDao = memberDao();
+	}
+	
 	public static void main(String[] args) {
 		// MemberRegisterSrevice가 DB처리를 위해 MemberDao클래스를 사용하고있다.
 		// 이렇게 한 클래스가 다른 클래스의 메서드를 실행할 때 이를 '의존'한다고 표현한다.
-//		private MemberDao memberDao = new MemberDao();
-//		
-//		public void regist(RegisterRequest req) {
-//			Member member = memberDao.selectByEmail(req.getEmail());
-//			if(member != null) {
-//				throw new DuplicateMemberException();
-//			}
-//			
-//			Member newMember = new Member(req.getEmail(), req.getPassword(), req.getName(), LocalDateTime.now());
-//			memberDao.insert(newMember);
-//		}
+		private MemberDao memberDao = new MemberDao();
+		
+		public void regist(RegisterRequest req) {
+			Member member = memberDao.selectByEmail(req.getEmail());
+			if(member != null) {
+				throw new DuplicateMemberException();
+			}
+			
+			Member newMember = new Member(req.getEmail(), req.getPassword(), req.getName(), LocalDateTime.now());
+			memberDao.insert(newMember);
+		}
 
 	}
 
